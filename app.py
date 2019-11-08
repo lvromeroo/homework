6b222e016aa6849b01161756ffb120b11cb08302
lvromeroo <lvromeroo@unal.edu.co>
2019-11-08 16:53:56 -0500

    Lizet

--- app.py
+++ app.py
@@ -353,4 +353,4 @@ def update_balance(exchange, leverage, start_date, end_date, data):
     }
 #-------------RUN SERVER---------------------------------------------------------
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0')
